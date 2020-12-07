019f895ff53c1eb09323e0790e37449900152f58
Gallaguz <gallaguz@gmail.com>
2020-12-07 06:11:06 +0300

    add new record. save in farebase

--- src/store/info.js
+++ src/store/info.js
@@ -16,13 +16,27 @@ export default {
     async fetchInfo ({dispatch, commit}) {
       try {
         const uid = await dispatch('getUid');
-        const info = (await firebase.database().ref(`/users/${uid}/info`)
+        const info = (await firebase.database()
+          .ref(`/users/${uid}/info`)
           .once('value')).val();
         commit('setInfo', info);
       } catch (e) {
-
+        commit('setError');
+        throw e;
       }
-
+    },
+    async updateInfo ({dispatch, commit, getters}, toUpdate) {
+      try {
+        const uid = await dispatch('getUid');
+        const updateData = {...getters.info, ...toUpdate};
+        await firebase.database()
+          .ref(`/users/${uid}/info`)
+          .update(toUpdate);
+        commit('setInfo', updateData);
+      } catch (e) {
+        commit('setError');
+        throw e;
+      }
     }
   },
   getters: {
--- src/views/Record.vue
+++ src/views/Record.vue
@@ -4,18 +4,41 @@
       <h3>Новая запись</h3>
     </div>
 
-    <form class="form">
-      <div class="input-field" >
-        <select>
+    <Loader v-if="loading"/>
+
+    <p
+      v-else-if="!categories.length"
+      class="center"
+    >
+      Категорий пока нет.
+      <router-link to="/categories">Добавить категорию</router-link>
+    </p>
+    <form
+      v-else
+      class="form"
+      @submit.prevent="submitHandler"
+    >
+      <div class="input-field">
+        <select
+          v-model="category"
+          id="categorySelect"
+          ref="select"
+        >
           <option
-          >name cat</option>
+            v-for="category in categories"
+            :key="category.id"
+            :value="category.id"
+          >
+            {{ category.title }}
+          </option>
         </select>
-        <label>Выберите категорию</label>
+        <label for="categorySelect">Выберите категорию</label>
       </div>
 
       <p>
         <label>
           <input
+            v-model="type"
             class="with-gap"
             name="type"
             type="radio"
@@ -28,6 +51,7 @@
       <p>
         <label>
           <input
+            v-model="type"
             class="with-gap"
             name="type"
             type="radio"
@@ -39,21 +63,34 @@
 
       <div class="input-field">
         <input
+          v-model.number="amount"
           id="amount"
           type="number"
+          :class="{invalid: $v.amount.$dirty && !$v.amount.minValue}"
         >
         <label for="amount">Сумма</label>
-        <span class="helper-text invalid">amount пароль</span>
+        <span
+          class="helper-text invalid"
+          v-if="$v.amount.$dirty && !$v.amount.minValue"
+        >
+            Минимальная величина {{ $v.amount.$params.minValue.min }}
+          </span>
       </div>
 
       <div class="input-field">
         <input
+          v-model="description"
           id="description"
           type="text"
+          :class="{invalid: $v.description.$dirty && !$v.description.required}"
         >
         <label for="description">Описание</label>
         <span
-          class="helper-text invalid">description пароль</span>
+          class="helper-text invalid"
+          v-if="$v.description.$dirty && !$v.description.required"
+        >
+            Введите описание
+          </span>
       </div>
 
       <button class="btn waves-effect waves-light" type="submit">
@@ -65,11 +102,89 @@
 </template>
 
 <script>
+import {minValue, required} from "vuelidate/lib/validators";
+import {mapGetters} from 'vuex';
+
 export default {
   name: "Record",
-  mounted() {
+  data: () => ({
+    loading: true,
+    select: null,
+    categories: [],
+    category: null,
+    type: 'outcome',
+    amount: 1,
+    description: ''
+  }),
+  validations: {
+    amount: {
+      minValue: minValue(1)
+    },
+    description: {
+      required
+    }
+  },
+  async mounted() {
+    this.categories = await this.$store.dispatch('fetchCategories');
+    this.loading = false;
 
-  }
+    if (this.categories) {
+      this.category = this.categories[0].id;
+    }
+
+    setTimeout(() => {
+      this.select = M.FormSelect.init(this.$refs.select, {});
+      M.updateTextFields();
+    }, 0);
+  },
+  computed: {
+    ...mapGetters(['info']),
+    canCreateRecord() {
+      if (this.type === 'income') {
+        return true;
+      }
+
+      return this.info.bill >= this.amount;
+    }
+  },
+  methods: {
+    async submitHandler() {
+      if (this.$v.$invalid) {
+        this.$v.$touch();
+        return
+      }
+
+      try {
+        if (this.canCreateRecord) {
+          await this.$store.dispatch('createRecord', {
+            categoryId: this.category,
+            amount: this.amount,
+            description: this.description,
+            type: this.type,
+            date: new Date().toJSON()
+          });
+
+          const bill = this.type === 'income'
+            ? this.info.bill + this.amount
+            : this.info.bill - this.amount
+
+          await this.$store.dispatch('updateInfo', {bill});
+          this.$message('Запись успешно создана');
+          this.$v.$reset();
+          this.amount = 1;
+          this.description = '';
+        } else {
+          this.$message(`Недостаточно средств на счёте: ${this.amount - this.info.bill}`);
+        }
+      } catch (e) {
+      }
+    }
+  },
+  beforeDestroy() {
+    if (this.select && this.select.destroy) {
+      this.select.destroy();
+    }
+  },
 }
 </script>
 
